733badc35132dffcd1ded67c77b4bd316cc69843
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-10 20:22:22 +0300

    Второе задание

--- src/Main.java
+++ src/Main.java
@@ -8,9 +8,44 @@ public class Main {
             System.out.println("Установите версию приложения для iOS по ссылке");
         }
 
-        else {
+        else if (clientOS == 1) {
             System.out.println("Установите версию приложения для Android по ссылке");
         }
+
+        else {
+
+            System.out.println("Нет возможности идентифицировать операционную систему");
+        }
+
+        int clientDeviceYear = 2010;
+        int clientOS2 = 1;
+
+        if (clientOS2 == 0) {
+
+            if (clientDeviceYear >= 2015) {
+
+                System.out.println("Установите версию приложения для iOS по ссылке");
+            }
+
+            else {
+                System.out.println("Установите облегченную версию приложения для iOS по ссылке");
+            }
+
+
+        }
+        else if (clientOS2 == 1) {
+
+            if (clientDeviceYear < 2015) {
+
+                System.out.println("Установите облегченную версию приложения для Android по ссылке");
+            }
+
+            else {
+
+                System.out.println("Установите версию приложения для Android по ссылке");
+            }
+        }
+
 
 
     }
