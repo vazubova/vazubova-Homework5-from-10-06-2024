255eb48609a34d74a106e08012dff7efc2d67360
varvaragr <grechishnikova.varvara@gmail.com>
2024-06-10 08:15:41 +0300

    Первое задание

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,20 @@
 public class Main {
     public static void main(String[] args) {
 
+        int clientOS = 1;
+
+        if (clientOS == 0) {
+
+            System.out.println("Установите версию приложения для iOS по ссылке");
+        }
+
+        else {
+            System.out.println("Установите версию приложения для Android по ссылке");
+        }
+
 
     }
+
+
+
 }
